74504f379d82b042f899b0079e72eae2397720ed
anand kumar <ankm335@gmail.com>
2024-07-13 23:42:24 +0530

    build: practice

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,4 +15,4 @@ module.exports = withMT({
     },
   },
   plugins: [],
-});
+};
